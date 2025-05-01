ebf08aa673c838d17d3aa2ad218e9e862284d42d
0silo0 <nikita.vershinin2003@mail.ru>
2025-05-02 02:25:23 +0500

    Пока убраны unit тесты и закомменчены все блоки с admin в ci/cd

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders auth page by default', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  
+  // Проверяем существующие элементы из вашего App.tsx
+  expect(screen.getByText(/login/i)).toBeInTheDocument();
 });
