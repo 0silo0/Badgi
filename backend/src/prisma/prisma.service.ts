8d6bb7abdeb64e8f68d2abf51bf953cade8f45ee
0silo0 <nikita.vershinin2003@mail.ru>
2025-12-21 23:26:41 +0300

    Перевел провект на новую версию prisma v7.2, старая v6.6

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,17 +1,27 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    // 1. Создаем пул соединений PostgreSQL
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    // 2. Создаем адаптер Prisma для этого пула
+    const adapter = new PrismaPg(pool);
+    // 3. Передаем адаптер в родительский конструктор
+    super({ adapter });
+  }
+  
   async onModuleInit() {
     await this.$connect();
   }
 
   enableShutdownHooks(app: INestApplication) {
-    process.on('beforeExit', () => {
-      app.close().catch((error) => {
-        console.error('Error during application shutdown:', error);
-      });
+    process.on('beforeExit', async () => {
+      await this.$disconnect();
+      await app.close();
     });
   }
 }
